a22a566c7288004eb2640ff08cdf87ecf0c75b9b
abaksheeva <anna.semenova.sam@gmail.com>
2023-12-27 15:33:34 +0100

    - allure report improvements

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
+import os
+import platform
 from pathlib import Path
+
+import allure
 import pytest
 
 from model_generators.pet_generator import PetGenerator
@@ -10,6 +14,7 @@ def get_project_root_dir():
     return Path(__file__).absolute().parent.parent
 
 
+@allure.title("Create a new pet for the test")
 @pytest.fixture
 def create_pet():
     pet = PetGenerator.generate_new_pet()
@@ -17,3 +22,15 @@ def create_pet():
     assert_status_code(status_code)
     yield pet
 
+
+@pytest.fixture(scope="session", autouse=True)
+def create_env_file():
+    yield
+    envs = [
+        f"os_platform = {os.name}",
+        f"\nos_release = {platform.release()}",
+        f"\nos_version = {platform.version()}",
+        f"\npython_version = {platform.python_version()}"
+    ]
+    with open("allure-results/environment.properties", mode="w") as f:
+        f.writelines(envs)
